00afaadd3e69c68cd03b8ada5b6d9154824c610e
Rydozz15 <juan.fuentes@uc.cl>
2024-01-27 14:21:23 -0300

    Change the catch response from array to object

--- Front-End/src/views/Profile.jsx
+++ Front-End/src/views/Profile.jsx
@@ -1,37 +1,38 @@
-import axios from 'axios'
-import Context from '../contexts/Context'
-import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { ENDPOINT } from '../config/constans'
+import axios from "axios";
+import Context from "../contexts/Context";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ENDPOINT } from "../config/constans";
 
 const Profile = () => {
-  const navigate = useNavigate()
-  const { getDeveloper, setDeveloper } = useContext(Context)
+  const navigate = useNavigate();
+  const { getDeveloper, setDeveloper } = useContext(Context);
 
   const getDeveloperData = () => {
-    const token = window.sessionStorage.getItem('token')
-    axios.get(ENDPOINT.users, { headers: { Authorization: `Bearer ${token}` } })
-      .then(({ data: [user] }) => setDeveloper({ ...user }))
+    const token = window.sessionStorage.getItem("token");
+    axios
+      .get(ENDPOINT.users, { headers: { Authorization: `Bearer ${token}` } })
+      .then(({ data: { user } }) => setDeveloper({ ...user })) //[] original
       .catch(({ response: { data } }) => {
-        console.error(data)
-        window.sessionStorage.removeItem('token')
-        setDeveloper(null)
-        navigate('/')
-      })
-  }
+        console.error(data);
+        window.sessionStorage.removeItem("token");
+        setDeveloper(null);
+        navigate("/");
+      });
+  };
 
-  useEffect(getDeveloperData, [])
+  useEffect(getDeveloperData, []);
 
   return (
-    <div className='py-5'>
+    <div className="py-5">
       <h1>
-        Bienvenido <span className='fw-bold'>{getDeveloper?.email}</span>
+        Bienvenido <span className="fw-bold">{getDeveloper?.email}</span>
       </h1>
       <h3>
         {getDeveloper?.rol} en {getDeveloper?.lenguage}
       </h3>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
